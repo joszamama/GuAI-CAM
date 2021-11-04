931eab5d84fad099521de58611eba9e16f4b900f
joszamama <joszamama@gmail.com>
2021-11-04 19:19:16 +0100

    Cleaning up the repo and styling code, also added gitignore

--- camera/face_detection.py
+++ camera/face_detection.py
@@ -7,9 +7,10 @@ from send_email import send_email
 
 def start_recording():
     cap = cv2.VideoCapture(0)
+    
     face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    # VAR.RECEIVER = input("Correo electrónico: ")
+    
     detection = False
     detection_stopped_time = None
     timer_started = False
@@ -18,7 +19,6 @@ def start_recording():
 
     while True:
         _, frame = cap.read()
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray,  # Frame a analizar.
                                             1.05,  # Entre 1 y 1.5, menor es más certero y más lento.
@@ -33,6 +33,7 @@ def start_recording():
                 output = cv2.VideoWriter(
                     f"recordings/{current_time}.mp4", fourcc, VAR.FRAME_RATE, frame_size)
                 print("Grabando...")
+        
         elif detection:
             if timer_started:
                 if time.time() - detection_stopped_time >= VAR.SECONDS_TO_WAIT:
@@ -40,16 +41,17 @@ def start_recording():
                     timer_started = False
                     output.release()
                     print("Fin de la grabación")
-                    send_email("http://host/directorio/" + current_time + ".mp4")
+                    send_email(VAR.MESSAGE + "http://host/directorio/" + current_time + ".mp4")
             else:
                 timer_started = True
                 detection_stopped_time = time.time()
-            
+        
+
         if detection:
             output.write(frame)
-
+        
         cv2.imshow("Camera", frame)
-
+        
         if cv2.waitKey(1) == ord('q'):
             break
 
--- camera/interface.py
+++ camera/interface.py
@@ -1,18 +1,17 @@
 from face_detection import start_recording
 from tkinter import *
-from tkinter import messagebox
 
 
 def ventana_principal():
     raiz = Tk()
     menu = Menu(raiz)
 
-    #DATOS
+    # CÁMARA
     menudatos = Menu(menu, tearoff=0)
-    menudatos.add_command(label="Cargar", command=start_recording)
-    menudatos.add_command(label="Listar", command=ord('q'))
+    menudatos.add_command(label="Comenzar", command=start_recording)
+    menudatos.add_command(label="Detener", command=ord('q'))
     menudatos.add_command(label="Salir", command=raiz.quit)
-    menu.add_cascade(label="Datos", menu=menudatos)
+    menu.add_cascade(label="Cámara", menu=menudatos)
 
     raiz.config(menu=menu)
     raiz.mainloop()
